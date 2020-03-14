952d5bf257a338415273d8090f0f7ca849b389d7
600610721 <bang_dekdee@hotmail.co.th>
2020-03-14 21:55:39 +0700

    add sql

---
+++ sql/init.sql
@@ -0,0 +1,12 @@
+create schema if not exists rating collate utf8mb4_0900_ai_ci;
+
+create table if not exists Rate
+(
+	id int auto_increment
+		primary key,
+	studentId varchar(255) null,
+	courseId varchar(255) null,
+	rating float null,
+	constraint rate_studentId_courseId_uindex
+		unique (studentId, courseId)
+);
